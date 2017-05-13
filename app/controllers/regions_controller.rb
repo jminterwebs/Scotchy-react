97d737c5a6c3ffd4c896272d15b3f9bd5da4a3f6
Unknown <jmverdone@gmail.com>
2017-05-13 19:52:43 -0400

    set up region controller framework

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,2 +1,45 @@
 class RegionsController < ApplicationController
+  before_action :set_region!, except: [:create, :index, :new]
+
+
+  def index
+    @regions = Region.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @region = Region.new
+  end
+
+  def create
+    @region = Region.new
+
+  end
+
+
+  def update
+    @region.update(region_params)
+
+    if @region.save
+      redirect_to @region
+    else
+      reder :edit
+    end
+  end
+
+  private
+
+  def set_region!
+    @region = Region.find(params[:id])
+  end
+
+  def region_params
+    params.require(:region).permit(:country, :sub_region)
+  end
+
 end
