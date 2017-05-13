8993ff0f6ce0915d100d688220f4d42d74959faa
Unknown <jmverdone@gmail.com>
2017-05-13 15:58:34 -0400

    updated log in with facebook to pull users name

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,9 @@
+
 class StaticController < ApplicationController
 
   def index
+    
+    @users = User.all
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170513181416) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
