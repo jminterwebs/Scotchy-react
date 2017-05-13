9733fe232a589aba4ddcc444a24d7fc75386acd6
Unknown <jmverdone@gmail.com>
2017-05-13 14:13:07 -0400

    removed generated test files

--- test/controllers/whiskeys_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class WhiskeysControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/distiller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DistillerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
