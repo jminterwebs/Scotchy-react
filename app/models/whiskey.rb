6fbe3a78c48f37017882804f83a9d93f85706e8e
Unknown <jmverdone@gmail.com>
2017-05-13 19:25:15 -0400

    updating model for namespacing

--- app/models/whiskey.rb
+++ app/models/whiskey.rb
@@ -4,5 +4,19 @@ class Whiskey < ApplicationRecord
   belongs_to :distiller
 
 
-  
+  def distiller_name
+    self.distiller.name
+  end
+
+  def country
+    self.distiller.region.country
+  end
+
+  def sub_region
+    self.distiller.region.sub_region
+  end
+
+
+
+
 end
