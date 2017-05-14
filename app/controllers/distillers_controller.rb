6b58ba4cb757beadb02a509f326c566f6c4c5e23
Unknown <jmverdone@gmail.com>
2017-05-13 20:07:29 -0400

    set up Distiller controller skeleton

--- app/controllers/distillers_controller.rb
+++ app/controllers/distillers_controller.rb
@@ -1,2 +1,48 @@
 class DistillersController < ApplicationController
+  before_action :set_distiller!, except: [:create, :index, :new]
+
+
+  def index
+    @distillers = distiller.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @distiller = distiller.new
+  end
+
+  def create
+    @distiller = distiller.new
+
+  end
+
+
+  def update
+    @distiller.update(distiller_params)
+
+    if @distiller.save
+      redirect_to @distiller
+    else
+      reder :edit
+    end
+  end
+
+  private
+
+  def set_distiller!
+    @distiller = distiller.find(params[:id])
+  end
+
+  def distiller_params
+    params.require(:distiller).permit(:name)
+  end
+
+
+
+
 end
