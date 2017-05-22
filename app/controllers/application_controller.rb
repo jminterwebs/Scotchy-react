0d2dace51946d892f568f34508dd6dccd3e1e5d0
Unknown <jmverdone@gmail.com>
2017-05-21 21:54:42 -0400

    custom divise login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
   request.env['omniauth.origin'] || root_path
   end
+
+  protected
+
+       def configure_permitted_parameters
+           devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+           devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+       end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", sessions: 'users/sessions' }
   root 'static#index'
   resources :users
   resources :whiskeys
