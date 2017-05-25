e96fb3071b36ad922c76f935142cf64fe88c4b55
Unknown <jmverdone@gmail.com>
2017-05-24 21:38:30 -0400

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :whiskeys
     resources :distillers, only: [:show, :index]
-    resources :region
+
 
   end
 
   resources :whiskeys
     post '/whiskeys/:id/add' => 'whiskeys#add', :as => :add_whiskey
+    put '/whiskeys/:id/delete' => 'whiskeys#remove', :as => :remove_whiskey
   resources :distillers
   resources :regions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
