09855552b186eaab386548f9bb5cf1715a546cad
Unknown <jmverdone@gmail.com>
2017-07-27 20:49:54 -0400

    added prototype  for whiskey index page

--- app/assets/javascripts/whiskeys.js
+++ app/assets/javascripts/whiskeys.js
@@ -6,3 +6,45 @@
 //
 //
 // })
+
+$(document).on('turbolinks:load', function() {
+  testing()
+
+ })
+
+
+
+
+testing = () => {
+
+
+      $('nav a').on('click', function(event){
+        event.preventDefault()
+        console.log(this.className)
+      $.get(`/${this.className}`, function(data){
+        console.log(data)
+
+      })
+
+          // console.log(event)
+          //
+          // let indexLists = new IndexLists()
+          // console.log(indexLists.whiskeyList())
+      })
+
+
+}
+
+
+function IndexLists(data){
+
+}
+
+IndexLists.prototype.whiskeyList = function(){
+
+
+return ` <li> whiskey names </li>`
+
+
+
+}
