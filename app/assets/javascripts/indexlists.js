f1dcebf7779f192fffed0a47d4c85d5689462ebc
Unknown <jmverdone@gmail.com>
2017-07-31 19:39:32 -0400

    moved new whiskey form to hidden form in navbar

--- app/assets/javascripts/indexlists.js
+++ app/assets/javascripts/indexlists.js
@@ -9,19 +9,19 @@
 
 $(document).on('turbolinks:load', function() {
   indexMaker()
-
+  newWhiskeyForm()
+  addWhiskey()
  })
 
 
 
 
 indexMaker = () => {
-
-
       $('nav a').on('click', function(event){
         event.preventDefault()
 
         let className = this.className
+        if( className != "newWhiskey"){
       $.get(`/${className}`, function(data){
         let indexLists = new IndexLists()
 
@@ -38,13 +38,17 @@ indexMaker = () => {
               break;
           }
       })
+    }
       })
+
 }
 
 
 function IndexLists(data){
 
 }
+
+
 
 IndexLists.prototype.whiskeyList = function(data){
 
@@ -65,8 +69,6 @@ IndexLists.prototype.distillerList = function(data){
     for(let i=0; i <= data.length-1; i ++){
     distillerList.push(`<li> ${data[i].name} </li>`)
     }
-
-
   $('.indexList').empty().append(distillerList)
 
 }
@@ -74,13 +76,40 @@ IndexLists.prototype.distillerList = function(data){
 IndexLists.prototype.regionList = function(data){
 
   let regionList = []
-
-
     for(let i=0; i <= data.length-1; i ++){
     regionList.push(`<li> ${data[i].country} </li>`)
     }
-
-
   $('.indexList').empty().append(regionList)
 
 }
+
+
+
+function newWhiskeyForm(){
+
+
+
+  $('.newWhiskey').on('click', function(event){
+    event.preventDefault()
+
+    $('.newWhiskeyForm').show()
+
+  })
+
+
+
+
+}
+
+function addWhiskey(){
+  $('form').submit(function(event){
+    event.preventDefault()
+    console.log(event)
+    var values = $(this).serialize();
+    console.log(values)
+  })
+
+}
+
+//
+// <form><input type="hidden" value="${whiskey_id}" name="whiskey_id" id="whiskey_id"/> <input type="hidden" value="${user_id}" name="user_id" id="user_id"/> <textarea id="content" name="content"></textarea><input type="submit"/></form>`
