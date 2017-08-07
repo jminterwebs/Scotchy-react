dcac8b4f675a8e5057a744f40ef49cee8b16f2ec
Unknown <jmverdone@gmail.com>
2017-08-06 20:05:48 -0400

    next and previous both workins and looping

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -11,13 +11,13 @@ function showInfo(id) {
   function nextShowing(id){
     $.get(`/whiskeys/${id}`, function(data){
 
-    let whiskeyShow = `<div> <span onClick="showprev(${id})">Show Prev </span> <span onClick="nextInfo(${id})">Show Next </span>
+    let whiskeyShow = `<div> <span onClick="prevInfo(${id})">Show Prev </span> <span onClick="nextInfo(${id})">Show Next </span>
       <h1>${data.name}</h1>  <h2>Distiller: ${data.distiller.name} </h2>  <h3>Region: ${data.distiller.region_name}</h3> <li onclick="addtoFavorites(${data.id})"> Add to favorites </li></div>`
 
     $('.indexList').hide()
     $('.showList').empty().append(whiskeyShow)
 
-    console.log(whiskeyShow)
+
   })
 }
 
@@ -30,10 +30,28 @@ function showInfo(id) {
 
     var next = ids.indexOf(id) + 1
 
-    console.log(id + " id")
-    console.log(ids)
-    console.log(next + " current Index")
+    if (!ids[next]){
+      next = 0
 
+    }
+
+    nextShowing(ids[next])
+  }
+
+
+  function prevInfo(id) {
+
+    let ids =[]
+    for(let i = 0; i < $('.indexList li').length; i++ ) {
+      ids.push(parseInt($('.indexList li')[i].getAttribute("id")))
+    }
+
+    var next = ids.indexOf(id) - 1
+
+    if (!ids[next]){
+      next = ids.length -1
+
+    }
 
     nextShowing(ids[next])
   }
