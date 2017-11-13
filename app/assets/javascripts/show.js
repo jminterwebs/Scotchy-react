b3483ffb8101e48f1174926bfee6d7c41936eda1
Unknown <jmverdone@gmail.com>
2017-11-12 21:02:27 -0500

    clean up whitespace

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -12,17 +12,16 @@ function showInfo(id) {
     $.get(`/whiskeys/${id}`, function(data){
 
     let whiskeyShow = `<div> <span onClick="prevInfo(${id})">Show Prev </span> <span onClick="nextInfo(${id})">Show Next </span>
+
       <h1>${data.name}</h1>  <h2>Distiller: ${data.distiller.name} </h2>  <h3>Region: ${data.distiller.region_name}</h3> <li onclick="addtoFavorites(${data.id})"> Add to favorites </li></div>`
 
     $('.indexList').hide()
     $('.showList').empty().append(whiskeyShow)
-
-
+    
   })
 }
 
   function nextInfo(id) {
-
     let ids =[]
     for(let i = 0; i < $('.indexList li').length; i++ ) {
       ids.push(parseInt($('.indexList li')[i].getAttribute("id")))
