cb9edb49f268258641d4776e8a1a8a81d35168a7
Unknown <jmverdone@gmail.com>
2017-12-04 12:48:54 -0500

    whiskeys added to distiller

--- app/assets/javascripts/distillerShow.js
+++ app/assets/javascripts/distillerShow.js
@@ -4,16 +4,24 @@ function distillerShowInfo(id){
 
 
 function distillerNextShow(id){
+  let distillerWhiskeys = []
+
+
   $.get(`/distillers/${id}`, function(data){
-    console.log(data)
-    let distillerShow = `<div> <span onClick="prevInfo(${id})">Show Prev </span> <span onClick="nextInfo(${id})">Show Next </span>
+      for(let i =0; i < data.whiskeys.length; i ++){
+        distillerWhiskeys.push(`<li>${data.whiskeys[i].name}</li>`)
+      }
+    let distillerShow = `<div > <span onClick="prevInfo(${id})">Show Prev </span> <span onClick="nextInfo(${id})">Show Next </span>
         <h1>${data.name}</h1>
         <h3>Region: ${data.region_name}</h3>
+        <div class=class="whiskeyList"></div>
     </div>`
 
-
-     $('.indexList').hide()
-     $('.showList').empty().append(distillerShow)
+    $('.whiskeyList').empty().append(distillerWhiskeys)
+    $('.indexList').hide()
+    $('.favDistiller').hide()
+    $('.favWhiskey').hide()
+    $('.showList').empty().append(distillerShow)
 
 
   })
